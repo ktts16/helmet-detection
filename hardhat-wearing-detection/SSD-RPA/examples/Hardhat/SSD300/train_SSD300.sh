ee1a67f633f379c4736a2da2529d439d07521ad0
ktts16 <ktts16@hotmail.com>
2019-08-14 15:30:00 +0700

    Add command (in comment) for CPU only training

--- hardhat-wearing-detection/SSD-RPA/examples/Hardhat/SSD300/train_SSD300.sh
+++ hardhat-wearing-detection/SSD-RPA/examples/Hardhat/SSD300/train_SSD300.sh
@@ -2,5 +2,6 @@
 ./build/tools/caffe train -solver="examples/Hardhat/SSD300/solver.prototxt" \
 -weights="models/VGGNet/VGG_ILSVRC_16_layers_fc_reduced.caffemodel" \
 --gpu 0 2>&1 | tee examples/Hardhat/SSD300/SSD300_Hardhat.log
+# 2>&1 | tee examples/Hardhat/SSD300/SSD300_Hardhat.log
 
 \
