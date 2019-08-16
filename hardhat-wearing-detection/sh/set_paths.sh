aada602a49c7dff5424d5eb476a26fd7c56354f8
ktts16 <ktts16@hotmail.com>
2019-08-16 07:19:31 +0700

    Print paths

--- hardhat-wearing-detection/sh/set_paths.sh
+++ hardhat-wearing-detection/sh/set_paths.sh
@@ -1,6 +1,10 @@
 # set CAFFE_ROOT (environment variable)
 export CAFFE_ROOT=$HOME/github/helmet-detection/hardhat-wearing-detection/SSD-RPA
+echo 'CAFFE_ROOT:'
+echo $CAFFE_ROOT
 
 # append path to PYTHONPATH (environment variable)
 export PYTHONPATH=$PYTHONPATH:$CAFFE_ROOT/python
 # export PYTHONPATH=$CAFFE_ROOT/python
+echo 'PYTHONPATH:'
+echo $PYTHONPATH
