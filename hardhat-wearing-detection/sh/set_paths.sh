91ff79a81c7735165254f99fa663d068f01d2ab1
ktts16 <ktts16@hotmail.com>
2019-08-16 07:00:33 +0700

    Add sh script for setting CAFFE_ROOT and PYTHONPATH

---
+++ hardhat-wearing-detection/sh/set_paths.sh
@@ -0,0 +1,6 @@
+# set CAFFE_ROOT (environment variable)
+export CAFFE_ROOT=$HOME/github/helmet-detection/hardhat-wearing-detection/SSD-RPA
+
+# append path to PYTHONPATH (environment variable)
+export PYTHONPATH=$PYTHONPATH:$CAFFE_ROOT/python
+# export PYTHONPATH=$CAFFE_ROOT/python
